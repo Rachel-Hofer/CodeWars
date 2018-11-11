b39edd80356c5aa53cf6b4782484fc789eb65ce5
Rachel Hofer <rhofer3@gmail.com>
2018-11-11 10:59:15 -0500

    Challnge4-TranspoOnVacation

---
+++ Challange4-TransportationOnVacation/transpo.js
@@ -0,0 +1,16 @@
+let days = 1;
+
+function rentalCarCost(days) {
+  let total = days * 40;
+
+  if(days > 0 && days <= 2){
+    total
+    return total
+  }
+  if(days >= 3 && days <= 6){
+    return total -= 20
+  }
+  if(days >= 7){
+    return total -= 50
+  }
+}
