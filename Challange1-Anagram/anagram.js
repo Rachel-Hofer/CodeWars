3fb5708ea67dadb5e80702dd363bea8adf3b4e89
Rachel Hofer <rhofer3@gmail.com>
2018-11-11 11:09:38 -0500

    added instructions

--- Challange1-Anagram/anagram.js
+++ Challange1-Anagram/anagram.js
@@ -1,8 +1,16 @@
 
+// Instructions
+// An anagram is a word, phrase, or sentence formed from another by rearranging its letters. 
+// An example of this is "angel", which is an anagram of "glean".
+// Given an array of words write a method that will return the total number of different anagrams inside that array. 
+// You can only count once the anagram between 2 words. 
+// For instance, in the previous example we show "angel" and "glean" as anagrams, but you don't have to count the opposite between "glean" and "angel".
+// Some examples:
+// There are 2 anagrams in the array ["dell", "ledl", "abc", "cba"]
+// There are 7 anagrams in the array ["dell", "ledl", "abc", "cba", "bca", "bac"]
+
+
 let words = ['dell', 'ledl', 'abc', 'cba', 'bca', 'bac', 'cab']
-
-
-
 
 function anagramCounter(arrayOfWords){
   let sortedWords = arrayOfWords.map(word=> word.split('').sort().join(''));
@@ -26,6 +34,3 @@ function anagramCounter(arrayOfWords){
 
 anagramCounter(words)
 
-
-
-Collapse
