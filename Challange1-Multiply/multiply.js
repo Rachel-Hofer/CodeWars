9f7394291180d91107db341474f76467059e161c
Rachel Hofer <rhofer3@gmail.com>
2018-11-11 11:14:04 -0500

    instructions added

---
+++ Challange1-Multiply/multiply.js
@@ -0,0 +1,11 @@
+// Instructions
+// The code does not execute properly. Try to figure out why.
+// multiply = function (a, b) {
+//     a * b;
+//   }
+
+
+
+multiply = function (a, b) {
+    return a * b;
+  }
