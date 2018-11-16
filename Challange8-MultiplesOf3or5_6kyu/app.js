1f15a37772855302c3beb7ffc3128a4eff05d889
Rachel Hofer <rhofer3@gmail.com>
2018-11-16 14:37:09 -0500

    Challange8

---
+++ Challange8-MultiplesOf3or5_6kyu/app.js
@@ -0,0 +1,17 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+// The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+
+function solution(number) {
+    var total = 0;
+    for (var i = 0; i < number; i++)
+    {
+        if (i % 3 === 0 || i % 5 === 0)
+        {
+            total += i;
+        }
+    }
+    return total;
+}
+
