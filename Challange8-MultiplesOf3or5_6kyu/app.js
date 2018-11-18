88f0982452d9edb85209b6f86cfd5db0ea5b9c2b
Rachel Hofer <rhofer3@gmail.com>
2018-11-17 23:51:51 -0500

    best practice added

--- Challange8-MultiplesOf3or5_6kyu/app.js
+++ Challange8-MultiplesOf3or5_6kyu/app.js
@@ -15,3 +15,17 @@ function solution(number) {
     return total;
 }
 
+
+
+// Best practice ansswer:
+
+// function solution(number){
+//     var sum = 0;
+    
+//     for(var i = 1;i< number; i++){
+//       if(i % 3 == 0 || i % 5 == 0){
+//         sum += i
+//       }
+//     }
+//     return sum;
+//   }
