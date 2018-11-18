8bdb35994950a12d885dfa223e1d41f957de02c4
Rachel Hofer <rhofer3@gmail.com>
2018-11-17 23:59:26 -0500

    best practice added

--- Challange4-ReverseSequence_8kyu/reverse.js
+++ Challange4-ReverseSequence_8kyu/reverse.js
@@ -1,3 +1,12 @@
 const reverseSeq = n => {
     return [...Array(n+1).keys()].slice(1, n+1).reverse();
   };
+
+  //Best Practice:
+
+  // const reverseSeq = n => {
+  //   let arr = [];
+  //     for (let i=n; i>0; i--) {
+  //       arr.push(i);
+  //       } return arr;
+  //   };
