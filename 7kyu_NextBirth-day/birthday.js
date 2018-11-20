e58de6df252b6cef4d0ec3e09a4239cd720622ba
Rachel Hofer <rhofer3@gmail.com>
2018-11-20 10:26:25 -0500

    complete

---
+++ 7kyu_NextBirth-day/birthday.js
@@ -0,0 +1,34 @@
+//Instructions:
+
+// Can you find after how many years will a person's birthday fall on the same day of the week that he was born?
+// For example, Joy's birthday is on 16th October, 1990 which falls on Friday. 
+// After how many years will his birthday fall on Friday again? (That would be 11 years)
+// nextBirthdayOfTheWeek(new Date(1990, 10, 16)) //11
+// nextBirthdayOfTheWeek(new Date(2012, 5, 20))  //6
+// nextBirthdayOfTheWeek(new Date(1975, 2, 22))  //5
+
+
+// My Answer:
+
+let nextBirthdayOfTheWeek = function(birthday) {
+    let weekday = birthday.getDay();
+    let yr = birthday.getFullYear();
+    yr += 1;
+    let month = birthday.getMonth();
+    let day = birthday.getDate();
+    
+    while( weekday !== new Date( yr, month, day ).getDay() ){
+      yr++;
+    }
+    return yr - birthday.getFullYear();
+  }
+  
+  
+  // Best Practice:
+  
+  // var nextBirthdayOfTheWeek = function(birthday) {
+  //   var dow = birthday.getDay(), bYear = birthday.getFullYear();
+  //   do birthday.setFullYear(birthday.getFullYear() + 1);
+  //   while(birthday.getDay() != dow);
+  //   return birthday.getFullYear() - bYear;
+  // };
