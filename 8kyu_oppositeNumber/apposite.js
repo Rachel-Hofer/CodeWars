b4d2f71e35727f23438e14189f9889c437fcfa0c
Rachel Hofer <rhofer3@gmail.com>
2018-11-22 18:03:25 -0500

    complete

---
+++ 8kyu_oppositeNumber/apposite.js
@@ -0,0 +1,17 @@
+// Instructions:
+
+// Very simple, given a number, find its opposite
+
+
+// My Answer:
+
+function opposite(number) {
+    return -number
+  }
+
+
+// Best Practice:
+
+//   function opposite(number) {
+//     return(-number);
+//   }
