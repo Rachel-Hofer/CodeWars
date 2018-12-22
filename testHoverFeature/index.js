49245d9aa7bc72bc8e2ec86eafd73e03787a9f3c
Rachel Hofer <rhofer3@gmail.com>
2018-12-22 14:26:24 -0500

    arr map

--- testHoverFeature/index.js
+++ testHoverFeature/index.js
@@ -1,8 +1,17 @@
-let myString = "as long as you hovering here";
+
+let  titleArr = ["this the first string"," this is second string", "this is the third string"]
 
 const stringCutter = str => {
-    let halfStr =   `${str.slice(0,str.length/2)}...`
-    str.length >= 6 ? console.log(halfStr):  console.log(str)  
+    let halfStr =   `${str.slice(0,str.length/2)}...`;
+    if(str.length >= 6 ){
+        return halfStr
+    } else {
+        return str
+    }
+
 }
-stringCutter(myString)
+
+let titleCompress = titleArr.map(eachTitle => {return stringCutter(eachTitle) });
+console.log(titleCompress)
+
 
