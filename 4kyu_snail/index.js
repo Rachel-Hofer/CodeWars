f20e27145a740ebca54c27f2bb7647d8216fe16a
Rachel Hofer <rhofer3@gmail.com>
2018-12-29 14:58:12 -0500

    passed all tests

--- 4kyu_snail/index.js
+++ 4kyu_snail/index.js
@@ -5,9 +5,24 @@
 
 // My Answer:
 
-
-
-
+let snail = function(arr) {
+    let result = [];
+    
+    while(arr.length > 0) {
+      result = result.concat.apply(result, arr.splice(0, 1));
+      
+      for(let i = 0; i < arr.length; i+=1) {
+        result = result.concat(arr[i].splice(arr.length));
+      }
+      let temp = [].concat.apply([], arr.splice(arr.length-1)).reverse();
+      result = result.concat.apply(result, temp);
+      
+      for(let i = arr.length-1; i >= 0; i-=1) {
+        result = result.concat(arr[i].splice(0, 1));
+      }
+    }
+    return result;
+  }
 
 
 // Best Practice:
