bea47c310ee0d22e73536aa3ee5536d4031e1c31
Rachel Hofer <rhofer3@gmail.com>
2019-01-28 22:25:08 -0500

    instructions added

--- 6kyu_ReplaceNounPhrasesWithPronouns/index.js
+++ 6kyu_ReplaceNounPhrasesWithPronouns/index.js
@@ -1,9 +1,43 @@
-// Instructions
+// Instructions:
 
+// A Noun Phrase is a phrase that can be replaced by a pronoun [he/she/it].
+// For example, in the sentence:
+// a girl ate the cookie
+// "A girl" is a Noun Phrase, so we can say "she ate the cookie." "The cookie" is also a Noun Phrase, so we can even say "she ate it."
+// Both of these Noun Phrases are made up of an auxiliary (aux) [the/a] followed by a Noun (N) [girl/cookie].
+// More complicated Noun Phrases can also include an arbitrary number of adjectives (adj). For example:
+// a big tall scary girl ate the cookie
+// where "big", "tall", and "scary" are all adjectives.
+// For our simplified model, we will define a Noun Phrase as an auxiliary, followed by 0 or more adjectives, followed by a Noun.
+// Write a function that takes a sentence (as a string of words separated by spaces), a list of pronouns, and a dictionary 
+// (mapping words to the grammatical categories aux, adj, and N). Return the sentence with all Noun Phrases replaced by pronouns, in the order given.
+
+// Assume:
+// sentences will contain only lower-case letters and spaces (no punctuation)
+// any words not included in the dictionary are not auxiliaries, adjectives, or nouns
+// the number of pronouns provided will be equal to the number of Noun Phrases in the sentence
 
 
 // My Answer:
 
+function replaceNounPhrases(sentence, pronouns, dictionary) {
 
 
-// Best Practice
+}
+
+let sentence = "a girl ate the cookie";
+let pronouns = ["she", "it"];
+let dictionary = {
+    'a': 'aux',
+    'the': 'aux',
+    'girl': 'N',
+    'cookie': 'N',
+    'big': 'adj',
+    'tall': 'adj',
+    'scary': 'adj'
+};
+
+replaceNounPhrases(sentence, pronouns, dictionary);
+
+
+// Best Practice:
