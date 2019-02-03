731b20b817d0b854b9f2f196f2f47465ffe38028
Rachel Hofer <rhofer3@gmail.com>
2019-02-02 22:44:08 -0500

    corrected and done

--- 6kyu_areTheyTheSame/index.js
+++ 6kyu_areTheyTheSame/index.js
@@ -1,14 +1,15 @@
 // Instructions:
 
-// Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) 
-// that checks whether the two arrays have the "same" elements, with the same multiplicities. 
-// "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
-
+// Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
 
 
 // My Answer:
 
 function comp(array1, array2) {
+    if (array1 === undefined || array1 === undefined) {
+        return false
+    }
+
     let score = [];
     let array1Squared = array1.map(x => x * x);
     array1Squared.sort((a, b) => a - b);
@@ -22,11 +23,23 @@ function comp(array1, array2) {
             return false
         }
     }
+
     if (score.length === array1Squared.length) {
         return true
     }
 }
 
-let array1 = [121, 144, 19, 161, 19, 144, 19, 11];
-let array2 = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
-comp(array1, array2);
+a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19];
+comp(a1, a2);
+
+
+
+// Best Practice:
+
+function comp(array1, array2) {
+    if (array1 == null || array2 == null) return false;
+    array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+    return array1.map(v => v * v).every((v, i) => v == array2[i]);
+}
+
